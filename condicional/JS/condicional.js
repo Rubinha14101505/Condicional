6896bd512d381fb146891525c6aba41381dc83c4
Rubinha14101505 <134503850+Rubinha14101505@users.noreply.github.com>
2023-08-30 09:54:21 -0300

    Add files via upload

---
+++ condicional/JS/condicional.js
@@ -0,0 +1,33 @@
+function media() {
+    var val1 = document.getElementById('valor1').value;
+    var val2 = document.getElementById('valor2').value;
+    var media = (parseInt(val1) + parseInt(val2)) / 2;
+
+if(media >= 6)
+if(media==10)
+
+alert ("o aluno atingiu a media" + media + " e foi aprovado com sucesso!");
+
+else
+alert ("parabens, aprovado! Media" + media);
+
+else 
+alert ("volta ano que vem!");
+
+}
+
+function comparar(){
+    var val1 = document.getElementById('valor1').value;
+    var val2 = document.getElementById('valor2').value;
+
+    if(val1==val2)
+    if(val1> val2)
+
+alert ('Os valores são iguais')
+else 
+alert ('O valor 1 é maior que o valor 2')
+
+else 
+alert ('O valor 2 é maior que o valor 1')
+
+}
